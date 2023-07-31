e48fae47041a2ac2744923f9ceab5b8ed6e18b57
Tom Sparrow <793763+sparrowt@users.noreply.github.com>
2023-07-31 14:35:47 +0100

    Tweaks to avoid overlapping with some things

--- jenkins-blueocean-show-all-stage-timings.user.js
+++ jenkins-blueocean-show-all-stage-timings.user.js
@@ -2,7 +2,7 @@
 // @name         Jenkins show all stage timings in Blue Ocean view
 // @namespace    https://github.com/sparrowt
 // @homepage     https://github.com/sparrowt/myuserscripts
-// @version      0.1
+// @version      0.3
 // @description  Show all stage timings without having to click on each stage
 // @author       Tom Sparrow
 // @updateURL    https://github.com/sparrowt/myuserscripts/raw/main/jenkins-blueocean-show-all-stage-timings.user.js
@@ -23,11 +23,11 @@ function process() {
             // Add it as a text label so it's always visible
             var label = document.createElementNS("http://www.w3.org/2000/svg", 'text');
             label.textContent = durationText;
-            label.setAttribute('x', '15');
-            label.setAttribute('y', '-5');
+            label.setAttribute('x', '14');
+            label.setAttribute('y', '-10');
             // Different colour if it is less than 1 minute
             var textColour = durationText.indexOf('m') == -1 ? 'grey' : 'blue'
-            label.setAttribute('style', `font-size: 10px; fill: ${textColour};`);
+            label.setAttribute('style', `font-size: 9px; fill: ${textColour};`);
             el.appendChild(label);
         }
     });
