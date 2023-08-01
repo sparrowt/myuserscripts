16db3cde19043654d4e8e4a84a0d71ab1b9bfd76
Tom Sparrow <793763+sparrowt@users.noreply.github.com>
2023-08-01 17:30:30 +0100

    Minor improvements

--- jenkins-blueocean-show-all-stage-timings.user.js
+++ jenkins-blueocean-show-all-stage-timings.user.js
@@ -19,14 +19,14 @@ function process() {
         var titles = el.getElementsByTagNameNS("http://www.w3.org/2000/svg", 'title')
         if (titles.length) {
             // Extract duration from tooltip ('title') element
-            var durationText = titles[0].textContent.replace('Passed in ', '')
+            var durationText = titles[0].textContent.replace('Passed in ', '').replace('Failed in', 'X').replace('but marked as unstable.', 'X');
             // Add it as a text label so it's always visible
             var label = document.createElementNS("http://www.w3.org/2000/svg", 'text');
             label.textContent = durationText;
             label.setAttribute('x', '14');
             label.setAttribute('y', '-10');
             // Different colour if it is less than 1 minute
-            var textColour = durationText.indexOf('m') == -1 ? 'grey' : 'blue'
+            var textColour = durationText.indexOf('m') == -1 ? 'grey' : 'blue';
             label.setAttribute('style', `font-size: 9px; fill: ${textColour};`);
             el.appendChild(label);
         }
@@ -38,7 +38,7 @@ function process() {
     'use strict';
     const initInterval = setInterval(() => {
         if (getElements()) {
-            setTimeout(process, 500);
+            setTimeout(process, 1000);
             clearInterval(initInterval);
         }
     }, 500);
