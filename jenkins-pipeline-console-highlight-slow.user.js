9943065f90f2c1d6c49c8b5050be3e49eab8a1f3
T Sparrow <793763+sparrowt@users.noreply.github.com>
2025-02-03 16:26:54 +0000

    Highlight really long steps in red

--- jenkins-pipeline-console-highlight-slow.user.js
+++ jenkins-pipeline-console-highlight-slow.user.js
@@ -2,7 +2,7 @@
 // @name         Jenkins highlight slow steps in Pipeline Console
 // @namespace    https://github.com/sparrowt
 // @homepage     https://github.com/sparrowt/myuserscripts
-// @version      0.1
+// @version      0.2
 // @description  Highlight steps which took a significant amount of time in the Pipeline Console view
 // @author       sparrowt
 // @match        https://yourjenkinsinstance.example.net/*job/*/pipeline-console/*
@@ -12,9 +12,14 @@
 
 function highlightSlowSteps() {
     document.getElementsBySelector('.log-card--text-duration').forEach(el => {
-        if (el.textContent.includes('min')) {
+        if (el.textContent.includes('hr')) {
+            // At least 1 hour
+            el.style.color = 'red'
+        } else if (el.textContent.includes('min')) {
+            // At least 1 minute
             el.style.color = 'yellow'
         } else if (/^\d\d+ sec/i.test(el.textContent)) {
+            // At least 10 seconds
             el.style.color = 'aqua'
         }
     });
