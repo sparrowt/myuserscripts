2ef855ff83ffea88b837183b0e94e6a9418818a9
T Sparrow <793763+sparrowt@users.noreply.github.com>
2025-12-22 14:23:21 +0000

    Update for latest Pipeline Graph View changes
    `pipeline-console` URL no longer exists since it was
    unified with the graph view into a single url.
    
    The element class and time format has changed too.

--- jenkins-pipeline-console-highlight-slow.user.js
+++ jenkins-pipeline-console-highlight-slow.user.js
@@ -2,25 +2,28 @@
 // @name         Jenkins highlight slow steps in Pipeline Console
 // @namespace    https://github.com/sparrowt
 // @homepage     https://github.com/sparrowt/myuserscripts
-// @version      0.2
+// @version      0.3
 // @description  Highlight steps which took a significant amount of time in the Pipeline Console view
 // @author       sparrowt
-// @match        https://yourjenkinsinstance.example.net/*job/*/pipeline-console/*
+// @match        https://yourjenkinsinstance.example.net/*job/*/pipeline-overview/*
 // @run-at       document-idle
 // @grant        none
 // ==/UserScript==
 
 function highlightSlowSteps() {
-    document.getElementsBySelector('.log-card--text-duration').forEach(el => {
+    document.getElementsBySelector('div.pgv-step-detail-header__actions > span').forEach(el => {
         if (el.textContent.includes('hr')) {
             // At least 1 hour
             el.style.color = 'red'
-        } else if (el.textContent.includes('min')) {
-            // At least 1 minute
+        } else if (el.textContent.endsWith('m')) {
+            // At least 10 mins
             el.style.color = 'yellow'
-        } else if (/^\d\d+ sec/i.test(el.textContent)) {
+        } else if (/^\d+m \d+s/i.test(el.textContent)) {
+            // At least 1 min
+            el.style.color = 'cyan'
+        } else if (/^\d\d+s/i.test(el.textContent)) {
             // At least 10 seconds
-            el.style.color = 'aqua'
+            el.style.color = 'white'
         }
     });
 }
